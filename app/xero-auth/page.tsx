d461dbdcc4f610b8e9a3d7a8bd4cf356b1c5aa81
Ramsay <info@coresentia.com>
2025-07-27 14:00:13 +1000

    Create page.tsx
    Added Xero Auth page

---
+++ app/xero-auth/page.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+export default function XeroAuth() {
+  const clientId = 'C4A301AE7EAF486DB397F15E85E5FC78'
+  const redirectUri = encodeURIComponent('https://ivy.coresentia.com/api/xero/callback')
+  const scope = 'accounting.transactions accounting.contacts accounting.settings openid profile email offline_access'
+  
+  const authUrl = `https://login.xero.com/identity/connect/authorize?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}`
+  
+  return (
+    <div className="min-h-screen bg-black text-white flex items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-2xl mb-4">Xero Authentication</h1>
+        <p className="mb-4">This will authorize CoreSentia to create quotes in Xero</p>
+        <a 
+          href={authUrl}
+          className="bg-blue-500 hover:bg-blue-600 px-6 py-3 rounded inline-block"
+        >
+          Connect to Xero
+        </a>
+      </div>
+    </div>
+  )
+}

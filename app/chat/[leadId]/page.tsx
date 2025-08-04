466a62826627a120c7c6d5f889dee2515090970f
Ramsay <info@coresentia.com>
2025-08-04 20:46:36 +1000

    Update page.tsx
    Added background to Ivy

--- app/chat/[leadId]/page.tsx
+++ app/chat/[leadId]/page.tsx
@@ -2,14 +2,26 @@
 
 import { useParams } from 'next/navigation'
 import ChatInterface from '@/app/components/ChatInterface'
+import dynamic from 'next/dynamic'
+
+// Dynamically import NetworkCanvas (no SSR for canvas animation)
+const NetworkCanvas = dynamic(() => import('../../components/NetworkCanvas'), { ssr: false })
 
 export default function ChatPage() {
   const params = useParams()
   const leadId = params.leadId as string
 
   return (
-    <div className="min-h-screen bg-black">
-      <ChatInterface leadId={leadId} />
+    <div className="min-h-screen bg-black text-white relative overflow-hidden">
+      {/* Synapse canvas background */}
+      <div className="absolute inset-0 z-0">
+        <NetworkCanvas />
+      </div>
+
+      {/* Main content wrapper */}
+      <div className="relative z-10">
+        <ChatInterface leadId={leadId} />
+      </div>
     </div>
   )
 }

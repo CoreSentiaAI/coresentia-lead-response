479ce22433e42af664b05c1aa2fdff5e91718e69
Ramsay <info@coresentia.com>
2025-08-09 14:33:04 +1000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config) => {
-    // Don't bundle optional native deps of `ws` (pulled by supabase realtime)
-    config.externals = config.externals || []
-    config.externals.push({
-      'utf-8-validate': 'commonjs utf-8-validate',
-      bufferutil: 'commonjs bufferutil',
-    })
-    return config
-  },
-}
+const nextConfig = {}
 
 module.exports = nextConfig
